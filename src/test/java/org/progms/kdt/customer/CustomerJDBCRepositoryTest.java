050771d970be994edd562a0f9e2e2ec9622f3776
haegyeong <haegyeong.kim01@gmail.com>
2023-02-06 17:46:53 +0900

    [ADD] insert

--- src/test/java/org/progms/kdt/customer/CustomerJDBCRepositoryTest.java
+++ src/test/java/org/progms/kdt/customer/CustomerJDBCRepositoryTest.java
@@ -14,6 +14,10 @@ import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import javax.sql.DataSource;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.UUID;
+
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 
@@ -74,5 +78,29 @@ class CustomerJDBCRepositoryTest {
 
         var unknown = customerJDBCRepository.findByName("unknown-user");
         assertThat(unknown.isEmpty(), is(true));
+    }
+
+    @Test
+    @DisplayName("이메일로 고객을 조회할 수 있다. ")
+    public void testFindByEmail() throws InterruptedException {
+        var customer = customerJDBCRepository.findByEmail("new-user@gmail.com");
+        assertThat(customer.isEmpty(), is(false));
+
+        var unknown = customerJDBCRepository.findByEmail("unknown-user@gmail.com");
+        assertThat(unknown.isEmpty(), is(true));
+    }
+
+    @Test
+    @DisplayName("전체를 지운 후 고객을 추가할 수 있다. ")
+    public void testInsert() throws InterruptedException {
+        customerJDBCRepository.deleteAll();
+
+        //  Window는 정밀도가 밀리(3자리)
+        var newCustomer = new Customer(UUID.randomUUID(), "test-user10", "test-user10@gmail.com", LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS));
+        customerJDBCRepository.insert(newCustomer);   //insert 안 되면 customerJDBCRepository에서 throw 가 된다.
+
+        var retrievedCustomer = customerJDBCRepository.findById(newCustomer.getCustomerId());
+        assertThat(retrievedCustomer.isEmpty(), is(false));
+        assertThat(retrievedCustomer.get(), samePropertyValuesAs(newCustomer));   //samePropertyValuesAs 두 개가 같은지 비교
     }
 }
